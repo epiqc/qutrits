90eeec8206e5e96966388c35ea06e7099f960cc7
Casey Duckering <cduckering@gmail.com>
2018-10-05 14:43:56 -0500

    Add verified Plus K circuit (assuming a working carry circuit)

--- cirq/qutrit/__init__.py
+++ cirq/qutrit/__init__.py
@@ -12,12 +12,17 @@ vars().update(base_gates)
 vars().update(controlled_gates)
 del base_gates, controlled_gates
 
-from cirq.qutrit.ancilla_generation_gate import AncillaGen
-
 from cirq.qutrit.evaluate import (
     default_ternary_state,
     evaluate_ternary_circuit,
     trit_list_to_state,
     verify_gate,
     verify_gate_inverse,
+)
+
+from cirq.qutrit.ancilla_generation_gate import AncillaGen
+from cirq.qutrit.plus_k_gate import (
+    PlusKGate,
+    PlusKCarryGate,
+    PlusKUncarryAddGate,
 )
