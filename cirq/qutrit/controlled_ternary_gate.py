f02b3f5569c2d5ad72bcd9bbe4561b8ad9ec307f
Pranav Gokhale <pranavgokhale@uchicago.edu>
2018-11-21 20:05:42 -0600

    break controlled ternary gate into a composite gate, based on fig 11 in elementary gates paper

--- cirq/qutrit/controlled_ternary_gate.py
+++ cirq/qutrit/controlled_ternary_gate.py
@@ -3,9 +3,12 @@ import itertools
 
 from cirq import ops
 from cirq.qutrit import raw_types
+from cirq.qutrit import common_gates
+import math, numpy as np
 
 
 class ControlledTernaryGate(raw_types.TernaryLogicGate,
+                            ops.CompositeGate,
                             ops.ReversibleEffect,
                             ops.TextDiagrammable):
     def __init__(self, base_gate, control_values):
@@ -16,6 +19,43 @@ class ControlledTernaryGate(raw_types.TernaryLogicGate,
         """
         self.base_gate = base_gate
         self.control_values = control_values
+
+        def validate_trits(self, trits):
+            pass
+
+        def applied_to_trits(self, trits):
+            pass
+
+    def default_decompose(self, qutrits):
+        # qutrits are control, target or control, control, target
+        assert len(self.control_values) in [1, 2]
+        assert len(qutrits) == len(self.control_values) + 1
+
+        if len(self.control_values) == 1:
+            if self.control_values == ((2,),) and self.base_gate == common_gates.F01:
+                yield C2F01()(*qutrits)
+            elif self.control_values == ((1,),) and self.base_gate == common_gates.PlusOne:
+                yield C1PlusOne()(*qutrits)
+            elif self.control_values == ((1,),) and self.base_gate == common_gates.MinusOne:
+                yield C1MinusOne()(*qutrits)
+            else:
+                assert False, "control_values: %s, base_gate: %s" % (self.control_values, self.base_gate)
+
+        elif len(self.control_values) == 2:
+            if self.control_values == ((1,), (1,)) and self.base_gate == common_gates.PlusOne:
+                yield C1C1PlusOne()(*qutrits)
+            elif self.control_values == ((1,), (1,)) and self.base_gate == common_gates.MinusOne:
+                yield C1C1MinusOne()(*qutrits)
+            elif self.control_values == ((2,), (2,)) and self.base_gate == common_gates.PlusOne:
+                yield C2C2PlusOne()(*qutrits)
+            elif self.control_values == ((2,), (2,)) and self.base_gate == common_gates.MinusOne:
+                yield C2C2MinusOne()(*qutrits)
+            else:
+                assert False, "control_values: %s, base_gate: %s" % (self.control_values, self.base_gate)
+
+        else:
+            assert False, 'more than two controls'
+
 
     @staticmethod
     def all_controlled_gates(base_gate, base_name, max_controls=1,
@@ -62,3 +102,174 @@ class ControlledTernaryGate(raw_types.TernaryLogicGate,
                                         trits[len(self.control_values):])
             trits[len(self.control_values):] = changed_trits
         return trits
+
+
+class C1C1PlusOne(raw_types.TernaryLogicGate,
+                  ops.CompositeGate):
+    def default_decompose(self, qutrits):
+        yield Ry12(-math.pi/4)(qutrits[2])
+        yield C1F12()(qutrits[1], qutrits[2])
+        yield Ry12(-math.pi/4)(qutrits[2])
+        yield C1F12()(qutrits[0], qutrits[2])
+        yield Ry12(math.pi/4)(qutrits[2])
+        yield C1F12()(qutrits[1], qutrits[2])
+        yield Ry12Pi4Ry01Pi4()(qutrits[2])
+        yield C1F01()(qutrits[1], qutrits[2])
+        yield Ry01(math.pi/4)(qutrits[2])
+        yield C1F01()(qutrits[0], qutrits[2])
+        yield Ry01(-math.pi/4)(qutrits[2])
+        yield C1F01()(qutrits[1], qutrits[2])
+        yield Ry01(-math.pi/4)(qutrits[2])
+
+
+class C2C2PlusOne(raw_types.TernaryLogicGate,
+                  ops.CompositeGate):
+    def default_decompose(self, qutrits):
+        yield Ry12(-math.pi/4)(qutrits[2])
+        yield C2F12()(qutrits[1], qutrits[2])
+        yield Ry12(-math.pi/4)(qutrits[2])
+        yield C2F12()(qutrits[0], qutrits[2])
+        yield Ry12(math.pi/4)(qutrits[2])
+        yield C2F12()(qutrits[1], qutrits[2])
+        yield Ry12Pi4Ry01Pi4()(qutrits[2])
+        yield C2F01()(qutrits[1], qutrits[2])
+        yield Ry01(math.pi/4)(qutrits[2])
+        yield C2F01()(qutrits[0], qutrits[2])
+        yield Ry01(-math.pi/4)(qutrits[2])
+        yield C2F01()(qutrits[1], qutrits[2])
+        yield Ry01(-math.pi/4)(qutrits[2])
+
+class C1C1MinusOne(raw_types.TernaryLogicGate,
+                   ops.CompositeGate):
+    def default_decompose(self, qutrits):
+        yield Ry01(math.pi/4)(qutrits[2])
+        yield C1F01()(qutrits[1], qutrits[2])
+        yield Ry01(math.pi/4)(qutrits[2])
+        yield C1F01()(qutrits[0], qutrits[2])
+        yield Ry01(-math.pi/4)(qutrits[2])
+        yield C1F01()(qutrits[1], qutrits[2])
+        yield Ry01MinusPi4Ry12MinusPi4()(qutrits[2])
+        yield C1F12()(qutrits[1], qutrits[2])
+        yield Ry12(-math.pi/4)(qutrits[2])
+        yield C1F12()(qutrits[0], qutrits[2])
+        yield Ry12(math.pi/4)(qutrits[2])
+        yield C1F12()(qutrits[1], qutrits[2])
+        yield Ry12(math.pi/4)(qutrits[2])
+
+class C2C2MinusOne(raw_types.TernaryLogicGate,
+                   ops.CompositeGate):
+    def default_decompose(self, qutrits):
+        yield Ry01(math.pi/4)(qutrits[2])
+        yield C2F01()(qutrits[1], qutrits[2])
+        yield Ry01(math.pi/4)(qutrits[2])
+        yield C2F01()(qutrits[0], qutrits[2])
+        yield Ry01(-math.pi/4)(qutrits[2])
+        yield C2F01()(qutrits[1], qutrits[2])
+        yield Ry01MinusPi4Ry12MinusPi4()(qutrits[2])
+        yield C2F12()(qutrits[1], qutrits[2])
+        yield Ry12(-math.pi/4)(qutrits[2])
+        yield C2F12()(qutrits[0], qutrits[2])
+        yield Ry12(math.pi/4)(qutrits[2])
+        yield C2F12()(qutrits[1], qutrits[2])
+        yield Ry12(math.pi/4)(qutrits[2])
+
+class Ry01(raw_types.TernaryLogicGate):
+    def __init__(self, theta):
+        self.theta = theta
+
+    def _unitary_(self):
+        theta = self.theta
+        return np.array([[math.cos(theta/2.0), -math.sin(theta/2.0), 0],
+                         [math.sin(theta/2.0), math.cos(theta/2.0), 0],
+                         [0, 0, 1]])
+
+class Ry12(raw_types.TernaryLogicGate):
+    def __init__(self, theta):
+        self.theta = theta
+
+    def _unitary_(self):
+        theta = self.theta
+        return np.array([[1, 0, 0],
+                         [0, math.cos(theta/2.0), -math.sin(theta/2.0)],
+                         [0, math.sin(theta/2.0), math.cos(theta/2.0)]])
+
+class Ry01MinusPi4Ry12MinusPi4(raw_types.TernaryLogicGate):
+    def _unitary_(self):
+        return np.dot(Ry12(-math.pi/4)._unitary_(), Ry01(-math.pi/4)._unitary_())
+
+class Ry12Pi4Ry01Pi4(raw_types.TernaryLogicGate):
+    def _unitary_(self):
+        return np.dot(Ry01(math.pi/4)._unitary_(), Ry12(math.pi/4)._unitary_())
+
+class C1F01(raw_types.TernaryLogicGate):
+    def _unitary_(self):
+        return np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 1, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 1, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                         [0, 0, 0, 1, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 1, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 1, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 1, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 1]])
+
+class C2F01(raw_types.TernaryLogicGate):
+    def _unitary_(self):
+        return np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 1, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 1, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 1, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 1, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 1, 0],
+                         [0, 0, 0, 0, 0, 0, 1, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 1]])
+
+class C1F12(raw_types.TernaryLogicGate):
+    def _unitary_(self):
+        return np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 1, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 1, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 1, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 1, 0, 0, 0],
+                         [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 1, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 1, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 1]])
+
+class C2F12(raw_types.TernaryLogicGate):
+    def _unitary_(self):
+        return np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 1, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 1, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 1, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 1, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 1, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 1],
+                         [0, 0, 0, 0, 0, 0, 0, 1, 0]])
+
+class C1PlusOne(raw_types.TernaryLogicGate):
+    def _unitary_(self):
+        return np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 1, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 1, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 1, 0, 0, 0],
+                         [0, 0, 0, 1, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 1, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 1, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 1]])
+
+
+class C1MinusOne(raw_types.TernaryLogicGate):
+    def _unitary_(self):
+        return np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 1, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 1, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 1, 0, 0, 0],
+                         [0, 0, 0, 1, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 1, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 1, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 1]])
